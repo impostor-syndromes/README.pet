92b9c225bc56bc7fda6ae53688aae1817b9b2ebe
yuto ichinose <yuto.0526.sano@gmail.com>
2024-04-06 14:39:54 +0900

    feat: 猫と皿を描画するところまで

--- cmd/README.pet/main.go
+++ cmd/README.pet/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"net/http" // httpパッケージをインポート
+
 	"github.com/gin-gonic/gin"
+	"pkg/generate-svg"
 )
 
 func main() {
@@ -10,6 +13,17 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	// 静的ファイルの配信
+	r.Static("/assets", "./assets")
+
+	r.GET("/show-svg", func(c *gin.Context) {
+		// Content-Typeを設定
+		c.Header("Content-Type", "image/svg+xml")
+
+		// SVGを返す
+		svg := generateSVG()
+		c.String(http.StatusOK, svg)
 	})
 	r.Run()
 }
