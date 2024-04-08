68a0013122ca1856124feef82706fc9c542a1e22
minusno0708 <naoyao1014@gmail.com>
2024-04-09 04:52:24 +0900

    feat:エフェクトの種類を追加

--- api/pkg/generate-svg.go
+++ api/pkg/generate-svg.go
@@ -5,7 +5,8 @@ import (
 )
 
 func GenerateSVG(number int) string {
-	dishImage, charaImage := chooseImages(number)
+	number = 11
+	dishImage, charaImage, effectImage := chooseImages(number)
 
 	const template = `<svg xmlns="http://www.w3.org/2000/svg">
 	<!-- 外側の丸みのある長方形 -->
@@ -20,24 +21,24 @@ func GenerateSVG(number int) string {
 	<!-- 猫画像 -->
 	<image x="170" y="20" width="90" height="80" href="/assets/charactors/%s" />
 	<!-- キラキラ -->
-	<image x="240" y="15" width="40" height="40" href="/assets/stars.png" />
+	<image x="240" y="15" width="40" height="40" href="/assets/effects/%s" />
   </svg>`
-	svg := fmt.Sprintf(template, dishImage, number, charaImage)
+	svg := fmt.Sprintf(template, dishImage, number, charaImage, effectImage)
 	return svg
 }
 
-func chooseImages(number int) (string, string) {
+func chooseImages(number int) (string, string, string) {
 	if number == 0 {
 		// 0コミット
-		return "empty.png", "neko_cry.png"
+		return "empty.png", "neko_cry.png", "clean.png"
 	} else if number <= 5 {
 		// 1~5コミット
-		return "small.png", "neko_normal.png"
+		return "small.png", "neko_normal.png", "clean.png"
 	} else if number <= 10 {
 		// 6~10コミット
-		return "medium.png", "neko_smile.png"
+		return "medium.png", "neko_smile.png", "stars_blinking.gif"
 	} else {
 		// 11コミット以上
-		return "large.png", "neko_happy.png"
+		return "large.png", "neko_happy.png", "stars.png"
 	}
 }
