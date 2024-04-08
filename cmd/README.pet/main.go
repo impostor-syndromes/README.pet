941e2c74aab268f6efb37b0c66af25af544dcc01
minusno0708 <naoyao1014@gmail.com>
2024-04-08 20:58:42 +0900

    feat:関数を結合

--- cmd/README.pet/main.go
+++ cmd/README.pet/main.go
@@ -18,11 +18,19 @@ func main() {
 	r.Static("/assets", "./assets")
 
 	r.GET("/show-svg", func(c *gin.Context) {
+		username := c.Query("username")
+
+		contributions, err := pkg.FetchContributions(username)
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+
 		// Content-Typeを設定
 		c.Header("Content-Type", "image/svg+xml")
 
 		// SVGを返す
-		svg := pkg.GenerateSVG(6)
+		svg := pkg.GenerateSVG(contributions[0])
 		c.String(http.StatusOK, svg)
 	})
 	r.Run()
