8aac01294cf6e2435f3e8ec34c9479508de8596a
minusno0708 <naoyao1014@gmail.com>
2024-04-11 16:19:03 +0900

    feat:作成した画像を組み合わせる

--- api/pkg/generate-svg.go
+++ api/pkg/generate-svg.go
@@ -14,13 +14,13 @@ func GenerateSVG(number int) string {
 	<!-- 内側の四角形 -->
 	<rect x="10" y="10" width="280" height="100" rx="20" ry="20" fill="none" stroke="white" stroke-width="5" />
 	<!-- 皿画像 -->
-	<image x="40" y="30" width="90" height="70" href="/assets/dishes/%s" />
+	<image x="-5" y="5" width="180" height="150" href="/assets/dishes/%s" />
 	<!-- 皿に表示する文字 -->
-	<text x="79" y="90">%d</text>
+	<text x="78" y="100">%d</text>
 	<!-- 猫画像 -->
-	<image x="170" y="20" width="90" height="80" href="/assets/charactors/%s" />
+	<image x="0" y="-135" width="400" height="400" href="/assets/charactors/%s" />
 	<!-- キラキラ -->
-	<image x="240" y="15" width="40" height="40" href="/assets/effects/%s" />
+	<image x="190" y="15" width="40" height="40" href="/assets/effects/%s" />
   </svg>`
 	svg := fmt.Sprintf(template, dishImage, number, charaImage, effectImage)
 	return svg
@@ -29,15 +29,15 @@ func GenerateSVG(number int) string {
 func chooseImages(number int) (string, string, string) {
 	if number == 0 {
 		// 0コミット
-		return "empty.png", "neko_cry.png", "clean.png"
+		return "baitdish-default.png", "cat-tears.png", "tears.gif"
 	} else if number <= 5 {
 		// 1~5コミット
-		return "small.png", "neko_normal.png", "clean.png"
+		return "baitdish-small.png", "cat-null-phase-2.png", "clean.png"
 	} else if number <= 10 {
 		// 6~10コミット
-		return "medium.png", "neko_smile.png", "stars_blinking.gif"
+		return "baitdish-medium.png", "cat-default.png", "stars1.gif"
 	} else {
 		// 11コミット以上
-		return "large.png", "neko_happy.png", "stars.png"
+		return "baitdish-large.png", "cat-smile.png", "stars2.gif"
 	}
 }
